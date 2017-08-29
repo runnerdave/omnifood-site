272c50e62f5f346854d670276961ca8e6e904002
runnerdave <runnerdave@gmail.com>
2017-08-29 17:19:42 +1000

    added css animation to the sections

--- resources/js/script.js
+++ resources/js/script.js
@@ -58,4 +58,28 @@ $(document).ready(function () {
             }
         });
 
+    /*Animations on scroll*/
+    $('.js--waypoint-1').waypoint(function (direction) {
+        $('.js--waypoint-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--waypoint-2').waypoint(function (direction) {
+        $('.js--waypoint-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--waypoint-3').waypoint(function (direction) {
+        $('.js--waypoint-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--waypoint-4').waypoint(function (direction) {
+        $('.js--waypoint-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
 });
